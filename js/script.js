8c12c9b6f9a54533b7f3007ea87d36bbfb46fd90
Ewa Paszkowska <ewqa89@gmail.com>
2022-11-01 12:33:31 +0100

    adding new tasks and showing if a task in done

--- js/script.js
+++ js/script.js
@@ -17,9 +17,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlSring;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();       
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim(); 
+
+    if (newTaskContent === "") {
+        return;
+    } 
+
+    addNewTask(newTaskContent);      
+};
+
     const init = () => {
 render();
-    };
+
+const form = document.querySelector(".js-form");
+        
+        form.addEventListener("submit", onFormSubmit); 
+};
 
     init();
 }
